94d3594cf5362547498815d3f6e876d4bf5a14ed
GokulBD <bdgokul05@gmail.com>
2022-04-04 12:24:07 +0530

    used passport

---
+++ config/passport.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const { Schema } = mongoose;
+
+const UsersSchema = new Schema({
+  username: String,
+  hash: String,
+  salt: String,
+});
+
+
+const Users = mongoose.model("Users",UsersSchema);
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "user[email]",
+      passwordField: "user[password]"
+    },
+    (username, password, done) => {
+      Users.findOne({ email })
+        .then((user) => {
+          if (!user || !user.validatePassword(password)) {
+            return done(null, false, {
+              errors: { "email or password": "is invalid" }
+            });
+          }
+
+          return done(null, user);
+        })
+        .catch(done);
+    }
+  )
+);
